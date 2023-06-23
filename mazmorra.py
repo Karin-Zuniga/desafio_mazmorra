233e516b8613e7008783c57978668f223ae434e3
karin-zuniga <karin.zuniga.munoz@gmail.com>
2023-06-22 22:34:35 -0400

    arreglando ciclos

--- mazmorra.py
+++ mazmorra.py
@@ -3,15 +3,28 @@
 #var_2 = jugador
 #var_3 = nivel ==> 3 niveles
 #F(x) 1 = historia
+#Alguna forma de evitar el error por ingreso de datos invalidos?
+import time
 import random
-def tirar_piedras(cantidad):
-    for i in range(0,11):
-        piedras_derecha = random.randint (0,cantidad)
-        piedras_izquierda = random.randint (0,cantidad)
-        piedras = int(input(f"Por la derecha vienen {piedras_derecha} y por la izquierda {piedras_izquierda}\nCuantas vienen?\n"))
-        piedras_totales = piedras_derecha + piedras_izquierda    
-        if piedras != piedras_totales:
-            break
+
+def historia(nivel):
+
+    puerta_primer_piso = str(input("Alrededor tuyo hay 4 puertas. \n Que puerta eligiras?\n1. Puerta Sur\n2. Puerta Norte\n3. Puerta Este\n4. Puerta Oeste\n"))
+    if puerta_primer_piso == "1":
+        habitacion_1_a(nivel)
+    elif puerta_primer_piso == "2":
+        habitacion_1_b(final)
+    elif puerta_primer_piso == "3":
+        habitacion_1_c(final)
+    elif puerta_primer_piso == "4":
+        habitacion_1_d(final)
+    elif puerta_primer_piso.lower() == "salir":
+        print("adios")
+        final = True
+    else:
+        print("opcion no valida. Para salir ingresa 'salir'")
+
+
 
 
 
@@ -25,12 +38,31 @@ def puertas(numero_puertas):
     puerta = input(f"Tienes {numero_puertas} puertas que dan{direccion} al este, y al oeste.\nHacia cual correras?\n1. Este\n2. Oeste\n{norte}")
     return puerta 
 
+def tirar_piedras(cantidad):
+    piedras_totales = 0
+    for i in range(0,11):
+        piedras_derecha = random.randint (0,cantidad)
+        piedras_izquierda = random.randint (0,cantidad)
+        piedras = int(input(f"Por la derecha vienen {piedras_derecha} y por la izquierda {piedras_izquierda}\nCuantas vienen?\n"))
+        suma_piedras = piedras_derecha + piedras_izquierda    
+        if piedras != suma_piedras:
+            print("Ouch! Eso debio doler.")
+        elif suma_piedras == piedras_totales:
+            piedras_totales =  piedras_totales + 1
+        else:
+            print("para salir ingresa '0'")
+
+    if piedras_totales >= 8:
+        print("Felicidades! Haz logrado llegar a la puerta.")
+    else:
+        print("Estas muy malherido para continuar. Mejor suerta para la proxima")
+
 def habitacion_1_a(nivel):
     print("Te encuentras en una habitacion con muros de piedra.")
     puerta = puertas(2)
     print("Corres lo mas rapido que puedes pero en mitad del camino pisas una trampa y una maquina en cada lado de la habitacion empieza a tirar piedras hacia ti. Mejor las cuentas las piedras para asegurarte que las esquivaste todas.")
     if nivel == "1":
-        tirar_piedras(10)
+        tirar_piedras(16)
             
 
     elif nivel == "2":
@@ -114,42 +146,23 @@ def habitacion_3_h(final):
     else:
         print("opcion no valida. Para salir ingresa 'salir'")        
 
-def historia(nivel):
-    final = False
-    while final == False:
-        puerta_primer_piso = str(input("Alrededor tuyo hay 4 puertas. \n Que puerta eligiras?\n1. Puerta Sur\n2. Puerta Norte\n3. Puerta Este\n4. Puerta Oeste\n"))
-        if puerta_primer_piso == "1":
-            habitacion_1_a(nivel)
-        elif puerta_primer_piso == "2":
-            habitacion_1_b(final)
-        elif puerta_primer_piso == "3":
-            habitacion_1_c(final)
-        elif puerta_primer_piso == "4":
-            habitacion_1_d(final)
-        elif puerta_primer_piso.lower() == "salir":
-            print("adios")
-            final = True
-        else:
-            print("opcion no valida. Para salir ingresa 'salir'")
-
-
 
 
 def main():
     nombre_juego = "Escape de la mazmorra"
+    final = False
     print(f"Bienvenido a {nombre_juego}\n")
+    time.sleep(1)
     jugador = input(f"Cual es tu nombre?\n")
-    
-    #preguntar si se pueden usar librerias... 
-    #import time
-    #time.sleep()
+    time.sleep(1)
     print(f"Hola {jugador}")
-    #time.sleep(1)
+    time.sleep(1)
     nivel = input(f"En que nivel deseas jugar(Ingresa el numero con tu alternativa.)\n1.Facil\n2. Medio\n3. Dificil\n")
 
     if nivel == "1" or nivel == "2" or nivel == "3":
-        #time.sleep()
+        time.sleep(2)
         print("Caminas por un bosque en medio de la noche. De repente, tus pies pisan una puerta. Por curiosidad, la abres y ves una escalera colgante, por lo que decides ir a investigar...\n...bajas lo que parecen ser tres pisos, y tan pronto como tus pies tocan el suelo, la puerta se cierra y la escalera cae.\n ...Parece ser que estas en un grave problema...")
+    while final == False:
         historia(nivel)
         
     else:
